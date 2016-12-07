754d7b441484eff19297cd0ab1690bdd5367d34e
Sultana Faria <sultanafaria.nstu@gmail.com>
2016-12-07 07:22:55 -0800

    palindrome number.c

---
+++ 07.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int num,temp,sum=0,r;
+    scanf("%d",&num);
+
+    temp = num;
+    while(num!=0){
+        r = num%10;
+        num = num/10;
+        sum = sum*10+r;
+    }
+    if(temp == sum)
+        printf("%d is a palindrome number\n",temp);
+    else
+        printf("%d is not a palindrome number\n",temp);
+
+    return 0;
+}
